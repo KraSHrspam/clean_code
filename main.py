c0aed5dc7540e4b4215e848355bb89d39043f222
KraSHrspam <70641065+KraSHrspam@users.noreply.github.com>
2022-12-20 18:24:57 +0300

    All edits
    удалил неиспользуемый код
    упростил сопровождение кода
    уточнил названия переменных
    отформатировал код
    показал чем отличаются переменные

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 import os
 import smtplib
 import textwrap as tw
+from dotenv import load_dotenv
 
 
 def readfile(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         text = file.read()
     return text
 
 
 if __name__ == '__main__':
-    headers_template = """
-    From: fuzedizeli@yandex.ru
-    To: fuzedizeli@yandex.ru
-    Subject: invite
-    Content-type: text/plain; charset="UTF-16";"""
-
+    load_dotenv()
     password = os.getenv('password')
     login = os.getenv('login')
-    finished_headers_template = tw.dedent(headers_template)
-    unfinished_letter = readfile('text.txt')
+    headers_template = f"""
+    From: {login}
+    To: {login}
+    Subject: invite
+    Content-type: text/plain; charset="UTF-8";"""
+
+    headers = tw.dedent(headers_template)
+    letter_template = readfile('text.txt')
     my_name, website = readfile('to_replace.txt').split('\n')
     finished_letter_sending = f'''\
-    {finished_headers_template}
+    {headers}
 
-    {unfinished_letter}
+    {letter_template}
     '''.replace('%my_name%', 'Илья').replace('%website%', 'dvmn.org')
 
-    finished_letter_sending = finished_letter_sending.encode('utf-16')
+    encoded_letter = finished_letter_sending.encode('utf-8')
     server = smtplib.SMTP_SSL('smtp.yandex.com:465')
     server.login(login, password)
-    server.sendmail(login, login, finished_letter_sending)
+    server.sendmail(login, login, encoded_letter)
     server.quit()
     print('Письмо отправлено!')
